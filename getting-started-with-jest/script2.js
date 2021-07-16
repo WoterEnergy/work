e7dae4b680c232ebd7ac9e6a02f88ca209a62042
WoterEnergy <marketolog@we.com.ua>
2021-07-16 11:53:33 +0300

    myOwnMission

--- getting-started-with-jest/script2.js
+++ getting-started-with-jest/script2.js
@@ -13,7 +13,7 @@
 
 
 ////// задача создать окно для ввода данных с диаметр, цвет, кномкой создать круг, и скорость анимации прикрутить действия на 
-//// что бы можно было управлять с клавиатуры 
+//// что бы можно было управлять с клавиатуры и доп вариант радио бтн чтобы он сам скакал
 
 
 let container = document.createElement('div')
@@ -33,3 +33,59 @@ button.className = 'btn'
 container.className = 'modal'
 container.append(size, color, speed, button)
 main.append(container)
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+
+function CriateCirkle(velX, velY, x, y,) {
+    // this.size = size;
+    // this.color = color;
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+
+}
+CriateCirkle.prototype.draw = function () {
+    ctx.beginPath()
+    ctx.fillStyle = color.value
+    ctx.arc(this.x, this.y, size.value, 0, 2 * Math.PI)
+    ctx.fill()
+    
+}
+
+CriateCirkle.prototype.update = function () {
+    if ((this.x + size) >= width) {
+        this.velX = -(this.velX);
+      }
+    
+      if ((this.x - size) <= 0) {
+        this.velX = -(this.velX);
+      }
+    
+      if ((this.y + size) >= height) {
+        this.velY = -(this.velY);
+      }
+    
+      if ((this.y - size) <= 0) {
+        this.velY = -(this.velY);
+      }
+    
+      this.x += this.velX;
+      this.y += this.velY;
+}
+
+
+let testBall = new CriateCirkle(20, 10, 10, 10);
+
+
+button.onclick = () => {
+    testBall.x
+    testBall.size
+    testBall.color
+    testBall.draw()
+
+}
+
