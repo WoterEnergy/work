043a7a5ea4ea3b0317857e04a9f53ca142ad0da0
WoterEnergy <marketolog@we.com.ua>
2021-07-30 11:24:55 +0300

    CheckIPdone

--- IPchecker/js/script.js
+++ IPchecker/js/script.js
@@ -1,45 +1,31 @@
-let btn = document.querySelector('.click')
-let text = document.querySelector('.info')
-
-btn.addEventListener('click', async function (event) {
-    fetch('https://api.ipify.org/?format=json')
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-        let IpInfo = data
-        let ipCheck = JSON.stringify(data)
-        //  text.append(ipCheck)
-        text.append( print(IpInfo))
-        // alert(ipCheck)
-      return console.log(IpInfo);
-    
-      
-    });
-    
-})
-
-const print = function(o){
-    let str='';
-
-    for(const p in o){
-        if(typeof o[p] == 'string'){
-            str+= "YOUR IP ADDRESS : " + o[p];
+class GetIp {
+    constructor () {
+        this.data = this.getInfo()
+        this.elements = {
+            btn: document.querySelector('.click'),
+            text: document.querySelector('.info')
         }
-        // else{
-        //     str+= p + ': {' + print(o[p]) + '}'
-        // }
     }
+    async getInfo () {
+        const ip = await fetch('https://api.ipify.org/?format=json')
+        const jsonIp = await ip.json()
+        const data = await fetch(`http://ip-api.com/json/${jsonIp.ip}?fields=country,regionName,city,zip,timezone,query`)
+        return data.json()
 
-    return str;
+    }
+    async render () {
+        const {btn, text} = this.elements
+        const {country, regionName, city, zip, timezone, query} = await this.data
+     
+        const empty = ''
+        const info = (`You info : Country - ${country},  region name - ${regionName}, city - ${city}, zip - ${zip}, time - ${timezone}, you ip ${query}`);
+        btn.addEventListener('click', () => {
+            text.innerHTML = ""
+            text.append(info)}
+     )
+    
+    }
 }
+const checkIP = new GetIp()
+checkIP.render()
 
-
-function dispData (data) {
-const output = data.forEach(element => {
-    str =''
-    str += `<div> IP is : ${ip} </div>`
-});
-
-document.body.innerHTML = str
-}
